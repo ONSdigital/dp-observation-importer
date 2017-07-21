d22ed0c3212c5c25d095b4a2cadba1e144e07944
Carl Hembrough <carl@bitwaves.co.uk>
2017-07-21 12:07:28 +0100

    Add error handler component. Currently just logs the error. The error needs to be serialised to a message with avro and sent to the message producer.

--- errors/handler.go
+++ errors/handler.go
@@ -1,11 +1,30 @@
 package errors
 
-import "github.com/ONSdigital/go-ns/log"
+import (
+	"github.com/ONSdigital/go-ns/log"
+)
 
+var _ Handler = (*KafkaHandler)(nil)
+
+// Handler defines a generic interface for handling errors.
 type Handler interface {
 	Handle(err error, data log.Data)
 }
 
+// KafkaHandler provides an error handler that writes to a Kafka error topic.
 type KafkaHandler struct {
-	//messageProducer MessageProducer
+	messageProducer MessageProducer
+}
+
+// MessageProducer dependency that writes messages
+type MessageProducer interface {
+	Output() chan []byte
+	Closer() chan bool
+}
+
+// Handle logs the error and sends is as a Kafka message.
+func (handler *KafkaHandler) Handle(err error, data log.Data) {
+	log.Error(err, data)
+
+	// todo - marshsal error + data to avro encoded message and send to handler.messageProducer.Output()
 }
