670dbdac72c2004cb08cacd506b100e93a67e3fa
Carl Hembrough <carl@bitwaves.co.uk>
2017-08-01 13:47:17 +0100

    Fix casing on mock import API.

--- dimension/store_test.go
+++ dimension/store_test.go
@@ -9,7 +9,7 @@ import (
 func TestStore_GetOrder(t *testing.T) {
 
 	data := "{\"headers\": [\"V4_1\",\"Data_Marking\",\"Time_codelist\"]}"
-	dataStore := NewStore("http://localhost:288100", dimensiontest.MockImportApi{Data: data})
+	dataStore := NewStore("http://localhost:288100", dimensiontest.MockImportAPI{Data: data})
 
 	Convey("Given a valid instanceId", t, func() {
 
@@ -27,7 +27,7 @@ func TestStore_GetOrder(t *testing.T) {
 
 func TestStore_GetOrderReturnAnError(t *testing.T) {
 
-	dataStore := NewStore("http://unknown-url:288100", dimensiontest.MockImportApi{FailRequest: true})
+	dataStore := NewStore("http://unknown-url:288100", dimensiontest.MockImportAPI{FailRequest: true})
 
 	Convey("Given a invalid URL", t, func() {
 
@@ -43,7 +43,7 @@ func TestStore_GetOrderReturnAnError(t *testing.T) {
 
 func TestIDCache_GetIDs(t *testing.T) {
 	data := "[{ \"dimension_name\": \"6_Year_1997\",\"value\": \"1997\",\"node_id\": \"123\"}]"
-	dataStore := NewStore("http://localhost:288100", dimensiontest.MockImportApi{Data: data})
+	dataStore := NewStore("http://localhost:288100", dimensiontest.MockImportAPI{Data: data})
 	Convey("Given a valid instance id", t, func() {
 		Convey("When the client api is called ", func() {
 			Convey("A list of dimensions are returned", func() {
